d81e4bf5196766e1fffa1001c587cd4d77aae1e4
Cedric Verstege <cedric.verstege@student.kit.edu>
2019-09-11 16:24:56 +0200

    Improve script for comparing unbinned and binned fits

--- examples/006_unbinned_fit/unbinned_binned.py
+++ examples/006_unbinned_fit/unbinned_binned.py
@@ -4,15 +4,14 @@ This example wants to illustrate when unbinned fits are used and when histogram 
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from scipy.stats import norm
+from multiprocessing import Pool
 
-from kafe2 import UnbinnedFit, UnbinnedPlotContainer
-from kafe2 import HistContainer, HistFit, HistPlotContainer
-from kafe2.fit.util.function_library import normal_distribution_pdf
+from kafe2 import UnbinnedFit
+from kafe2 import HistContainer, HistFit
 
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Rectangle
+
+np.random.seed(1009131719)
 
 
 def pdf(x, mu=0, sigma=1):
@@ -23,66 +22,102 @@ def cdf(x, mu=0, sigma=1):
     return norm(mu, sigma).cdf(x)
 
 
-size = 100000
-start = 4
-step = 100
-np.random.seed(1009131719)
-sample = np.random.standard_normal(size+1)
+class Fitters:
+    def __init__(self, size, low, high, steps):
+        self.data = self.gen_data(size)
+        self.steps = self.gen_steps(low, high, steps)
+        self.borders = (np.min(self.data), np.max(self.data))
+        self.n_bins = None
 
-# get parameters from fits
-u_fit = UnbinnedFit(sample, model_density_function=pdf, minimizer='scipy')
-h_data = []
-bins = [10, 50, 100]
-for i in bins:
-    data = HistContainer(n_bins=i, bin_range=(-3, 5), fill_data=sample[0:10])
-    h_data.append(data)
-h_fits = []
-for data in h_data:
-    fit = HistFit(data, model_density_function=pdf, model_density_antiderivative=cdf, minimizer='scipy')
-    h_fits.append(fit)
+    @staticmethod
+    def gen_data(size=100000):
+        return np.random.standard_normal(size)
 
-param_results = []
-error_results = []
-n_points = np.logspace(0.5, 5, num=step, dtype=int)
-#n_points = range(start-1, size, step)
-for j, i in enumerate(n_points):
-    print("Run {} of {}".format(j, len(n_points)))
-    u_fit.data = sample[0:i]
-    u_fit.do_fit()
-    params = [u_fit.parameter_values]
-    errors = [u_fit.parameter_errors]
-    for data, fit, n in zip(h_data, h_fits, bins):
-        data = HistContainer(n_bins=n, bin_range=(-3, 3), fill_data=sample[0:i])
-        fit.data = data
+    @staticmethod
+    def gen_steps(low, high, size, log=True):
+        if log:
+            low = np.log10(low)
+            high = np.log10(high)
+            return np.logspace(low, high, num=size, dtype=int)-1
+        else:
+            return np.linspace(low, high, num=size, dtype=int)-1
+
+    def do_unbinned(self, step):
+        data = self.data[0:step]
+        fit = UnbinnedFit(data, model_density_function=pdf)
         fit.do_fit()
-        params.append(fit.parameter_values)
-        errors.append(fit.parameter_errors)
-    param_results.append(params)
-    error_results.append(errors)
-param_results = np.array(param_results)
-error_results = np.array(error_results)
-results = [n_points]
-for i in range(len(param_results)):
-    results.append(param_results[:, i, 0])  # mu
-    results.append(error_results[:, i, 0])  # mu error
-    results.append(param_results[:, i, 1])  # sigma
-    results.append(error_results[:, i, 1])  # sigma error
-np.save('sample_fits', results)
-"""
+        params = fit.parameter_values
+        errors = fit.parameter_errors
+        return [params, errors]
 
-results = np.load('sample_fits.npy', allow_pickle=True)
-n_points = results[0]
-param_results = results[1]
-error_results = results[2]
-print(error_results)
-plt.xscale('log')
-plt.scatter(n_points, param_results[:, 0, 0]/error_results[:, 0, 0], label=r'$\mu$ Unbinned')
-plt.scatter(n_points, param_results[:, 1, 0]/error_results[:, 1, 0], label=r'$\mu$ Binned 10')
-plt.scatter(n_points, param_results[:, 2, 0]/error_results[:, 2, 0], label=r'$\mu$ Binned 50')
-plt.scatter(n_points, param_results[:, 3, 0]/error_results[:, 3, 0], label=r'$\mu$ Binned 100')
-plt.legend()
-plt.show()
-"""
+    def do_hist(self, step):
+        hist_cont = HistContainer(n_bins=self.n_bins, bin_range=self.borders, fill_data=self.data[0:step])
+        hist_fit = HistFit(hist_cont, model_density_function=pdf, model_density_antiderivative=cdf)
+        hist_fit.do_fit()
+        params = hist_fit.parameter_values
+        errors = hist_fit.parameter_errors
+        return [params, errors]
+
+    def do_fits(self):
+        p = Pool(processes=10)
+        _result = [p.map(self.do_unbinned, [i for i in self.steps])]
+        for n in [10, 50, 100]:
+            p = Pool(processes=10)
+            self.n_bins = n
+            _result.append(p.map(self.do_hist, [i for i in self.steps]))
+        return np.array(_result)
+
+
+if __name__ == '__main__':
+    fitters = Fitters(10000, 4, 10000, 50)
+    result = fitters.do_fits()
+    mu_lim = [-0.25, 0.25]
+    sig_lim = [0.3, 1.1]
+    fig, axs = plt.subplots(nrows=4, ncols=2, sharex=True)
+    ax = axs[0, 0]
+    ax.errorbar(fitters.steps, result[1, :, 0, 0], yerr=result[1, :, 1, 0], fmt='o')
+    ax.plot(fitters.steps, np.zeros(len(fitters.steps)), 'r--')
+    ax.set_ylim(mu_lim[0], mu_lim[1])
+    ax.set_title(r'10 Bins $\mu$')
+    ax = axs[0, 1]
+    ax.errorbar(fitters.steps, result[1, :, 0, 1], yerr=result[1, :, 1, 1], fmt='o')
+    ax.plot(fitters.steps, np.ones(len(fitters.steps)), 'r--')
+    ax.set_ylim(sig_lim[0], sig_lim[1])
+    ax.set_title(r'10 Bins $\sigma$')
+    ax = axs[1, 0]
+    ax.errorbar(fitters.steps, result[2, :, 0, 0], yerr=result[2, :, 1, 0], fmt='o')
+    ax.plot(fitters.steps, np.zeros(len(fitters.steps)), 'r--')
+    ax.set_ylim(mu_lim[0], mu_lim[1])
+    ax.set_title(r'50 Bins $\mu$')
+    ax = axs[1, 1]
+    ax.errorbar(fitters.steps, result[2, :, 0, 1], yerr=result[2, :, 1, 1], fmt='o')
+    ax.plot(fitters.steps, np.ones(len(fitters.steps)), 'r--')
+    ax.set_ylim(sig_lim[0], sig_lim[1])
+    ax.set_title(r'50 Bins $\sigma$')
+    ax = axs[2, 0]
+    ax.errorbar(fitters.steps, result[3, :, 0, 0], yerr=result[3, :, 1, 0], fmt='o')
+    ax.plot(fitters.steps, np.zeros(len(fitters.steps)), 'r--')
+    ax.set_ylim(mu_lim[0], mu_lim[1])
+    ax.set_title(r'100 Bins $\mu$')
+    ax = axs[2, 1]
+    ax.errorbar(fitters.steps, result[3, :, 0, 1], yerr=result[3, :, 1, 1], fmt='o')
+    ax.plot(fitters.steps, np.ones(len(fitters.steps)), 'r--')
+    ax.set_ylim(sig_lim[0], sig_lim[1])
+    ax.set_title(r'100 Bins $\sigma$')
+    ax = axs[3, 0]
+    ax.errorbar(fitters.steps, result[0, :, 0, 0], yerr=result[0, :, 1, 0], fmt='o')
+    ax.plot(fitters.steps, np.zeros(len(fitters.steps)), 'r--')
+    ax.set_ylim(mu_lim[0], mu_lim[1])
+    ax.set_title(r'Unbinned $\mu$')
+    ax = axs[3, 1]
+    ax.errorbar(fitters.steps, result[0, :, 0, 1], yerr=result[0, :, 1, 1], fmt='o')
+    ax.plot(fitters.steps, np.ones(len(fitters.steps)), 'r--')
+    ax.set_ylim(sig_lim[0], sig_lim[1])
+    ax.set_title(r'Unbinned $\sigma$')
+    plt.xscale('log')
+    plt.show()
+
+
 """
 improvements:
 scale: (ref-estimate)/error
@@ -91,28 +126,3 @@ parallelize fits
 save fit results, load when plotting
 check if empty bins have an influence on result: n_bins -> inf likelihood?
 """
-
-'''
-fig = p.figure
-ims = []
-for i in range(100, len(data)):
-    f.data = data[:i]
-    f.do_fit()
-    print(f.parameter_values)
-    p.plot()
-    # p.show_fit_info_box()
-    artists = list(p._artist_store[0].values())
-    im = []
-    # flatten the array
-    for artist in artists:
-        try:
-            artist = artist[0]
-        except TypeError:  # except indexing error for already flattened elements
-            pass
-        artist.set_animated(True)
-        im.append(artist)
-    ims.append(im)
-
-anim = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=100)
-plt.show()
-'''
