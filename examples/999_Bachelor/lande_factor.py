7e0b0f9a30b44c07027734e272e81b0b5428bc96
Cedric Verstege <cedric.verstege@student.kit.edu>
2019-11-03 16:46:51 +0100

    Add latex expressions

--- examples/999_Bachelor/lande_factor.py
+++ examples/999_Bachelor/lande_factor.py
@@ -4,10 +4,10 @@ from kafe2 import HistFit, HistContainer, Plot, MultiFit
 from six import iteritems
 
 def events_top(x, tau=2.2, k_top=1.0, a_bar_top=1.0, omega=1.0, delta=1.0, f_top=0.1):
-    return k_top*np.exp(-x/tau)+a_bar_top*np.exp(-x/tau)*np.cos(omega*x+delta)+f_top
+    return k_top*np.exp(-x/tau)*(1+a_bar_top*np.cos(omega*x+delta))+f_top
 
 def events_bot(x, tau=2.2, k_bot=1.0, a_bar_bot=1.0, omega=1.0, delta=1.0, f_bot=0.1):
-    return k_bot*np.exp(-x/tau)-a_bar_bot*np.exp(-x/tau)*np.cos(omega*x+delta)+f_bot
+    return k_bot*np.exp(-x/tau)*(1-a_bar_bot*np.cos(omega*x+delta))+f_bot
 
 def decay_top(x, tau=2.2, k_top=1.0, f_top=0.1):
     return k_top*np.exp(-x/tau)+f_top
@@ -19,12 +19,6 @@ def decay_bot(x, tau=2.2, k_bot=1.0, f_bot=0.1):
 class Lande:
     def __init__(self, low_limit=2, high_limit=16, b=5e-3, gen_data=False, fname="dpFilt_190325-0148.dat"):
         self.b = b
-        self.top_events = None
-        self.bot_events = None
-        self.hist_top = None
-        self.hist_bot = None
-        self.fit_top = None
-        self.fit_bot = None
         if gen_data:
             self._gen_data(1e6, low_limit, high_limit)
         else:
@@ -54,10 +48,10 @@ class Lande:
         print("Expected omega ist {}".format(omega))
         delta = 9.85  # phase delay
         if pars_top is None:
-            pars_top = {'tau': tau*1e6, 'k_top': 0.8, 'a_bar_top': 1e-3, 'omega': omega*1e-6, 'delta': delta,
+            pars_top = {'tau': tau*1e6, 'k_top': 0.8, 'a_bar_top': 0.00125, 'omega': omega*1e-6, 'delta': delta,
                         'f_top': 2.6e-3}
         if pars_bot is None:
-            pars_bot = {'tau': tau*1e6, 'k_bot': 0.7, 'a_bar_bot': 1e-2, 'omega': omega*1e-6, 'delta': delta,
+            pars_bot = {'tau': tau*1e6, 'k_bot': 0.7, 'a_bar_bot': 0.015, 'omega': omega*1e-6, 'delta': delta,
                         'f_bot': 1.5e-3}
         self.top_events = np.zeros(int(size), dtype=float)
         self.bot_events = np.zeros(int(size / 2), dtype=float)
@@ -82,16 +76,31 @@ class Lande:
         print("Done generating data.")
 
     def gen_fits(self, low_limit, high_limit):
-        self.hist_top = HistContainer(n_bins=100, bin_range=(low_limit, high_limit),
-                                      fill_data=self.top_events)
-        self.hist_bot = HistContainer(n_bins=100, bin_range=(low_limit, high_limit),
-                                      fill_data=self.bot_events)
-        self.fit_top = HistFit(self.hist_top, events_top, cost_function='nllr')
-        self.fit_bot = HistFit(self.hist_bot, events_bot, cost_function='nllr')
-        self.fit_multi = MultiFit((self.fit_top, self.fit_bot), minimizer='iminuit')
-        self.decay_fit_top = HistFit(self.hist_top, decay_top)
-        self.decay_fit_bot = HistFit(self.hist_bot, decay_bot)
-        self.decay_fit_multi = MultiFit((self.decay_fit_top, self.decay_fit_bot), minimizer='iminuit')
+        _hist_top = HistContainer(n_bins=100, bin_range=(low_limit, high_limit),
+                                  fill_data=self.top_events)
+        _hist_bot = HistContainer(n_bins=100, bin_range=(low_limit, high_limit),
+                                  fill_data=self.bot_events)
+        _fit_top = HistFit(_hist_top, events_top, cost_function='nllr')
+        _fit_bot = HistFit(_hist_bot, events_bot, cost_function='nllr')
+        shared_par_dict = {'x': 't', 'tau': r'\tau', 'omega': r'\omega', 'delta': r'\delta'}
+        top_par_names = {'k_top': r'K_0', 'a_bar_top': r'\bar{{A}}_0', 'f_top': 'f_0'}
+        bot_par_names = {'k_bot': r'K_1', 'a_bar_bot': r'\bar{{A}}_1', 'f_bot': 'f_1'}
+        top_par_names.update(shared_par_dict)
+        bot_par_names.update(shared_par_dict)
+        _fit_top.assign_model_function_latex_name("N_0")
+        _fit_top.assign_parameter_latex_names(**top_par_names)
+        _fit_top.assign_model_function_latex_expression(r'{k_top}\cdot\exp{{\left(\frac{{-{x}}}{{{tau}}}\right)}}'
+                                                        r'\left(1+{a_bar_top}\cdot\cos\left({omega}{x}+{delta}\right)\right)'
+                                                        r'+{f_top}')
+        _fit_bot.assign_model_function_latex_name("N_1")
+        _fit_bot.assign_parameter_latex_names(**bot_par_names)
+        _fit_bot.assign_model_function_latex_expression(r'{k_bot}\cdot\exp{{\left(\frac{{-{x}}}{{{tau}}}\right)}}'
+                                                        r'\left(1+{a_bar_bot}\cdot\cos\left({omega}{x}+{delta}\right)\right)'
+                                                        r'+{f_bot}')
+        self.fit_multi = MultiFit((_fit_top, _fit_bot), minimizer='iminuit')
+        _decay_fit_top = HistFit(_hist_top, decay_top)
+        _decay_fit_bot = HistFit(_hist_bot, decay_bot)
+        self.decay_fit_multi = MultiFit((_decay_fit_top, _decay_fit_bot), minimizer='iminuit')
 
     def decay_do_fit(self):
         self.decay_fit_multi.do_fit()
@@ -105,7 +114,6 @@ class Lande:
         self.fit_multi.set_parameter_values(**kwargs)
         for key, value in iteritems(limits):
             self.fit_multi.limit_parameter(key, value)
-        print(self.fit_multi.parameter_name_value_dict)
         self.fit_multi.do_fit()
         # self.fit_multi.report()
 
@@ -115,28 +123,6 @@ class Lande:
         # _plot = Plot((self.fit_top, self.fit_bot), separate_figures=True)
         _plot.plot()
 
-def compare_limits():
-    high_limits = np.linspace(12, 14, 20)
-    cost = np.zeros((len(high_limits), 2))
-    params = np.array([])
-    lande = Lande(low_limit=2, high_limit=19)
-    for i, lim in enumerate(high_limits):
-        lande.gen_fits(low_limit=2, high_limit=lim)
-        lande.do_fit(pre_fit=True, limits={'delta': (0, 2*np.pi)},
-                     **{'omega': 3, 'delta': np.pi, 'a_bar_top': 0.01, 'a_bar_bot': 0.01})
-        print()
-        for _par_name, _par_val, _par_err in zip(lande.fit_multi.parameter_names, lande.fit_multi.parameter_values,
-                                                 lande.fit_multi.parameter_errors):
-            print('%s: %.3E +- %.3E' % (_par_name, _par_val, _par_err))
-        params = np.append(params, list(zip(lande.fit_multi.parameter_names, lande.fit_multi.parameter_values,
-                                            lande.fit_multi.parameter_errors)))
-        for j, fit in enumerate(lande.fit_multi.fits):
-            print("cost/ndf: {}".format(fit.cost_function_value / fit._cost_function.ndf))
-            cost[i][j] = fit.cost_function_value / fit._cost_function.ndf
-
-    np.save("cost_check_around_min_min", [high_limits, params, cost])
-    plt.plot(high_limits, np.sum(cost, axis=1))
-    plt.show()
 
 def fit(lande):
     starting_values = {'omega': 3, 'delta': np.pi, 'a_bar_top': 1e-4, 'a_bar_bot': 1e-4}
@@ -156,15 +142,7 @@ def fit(lande):
     print("Ref g: {}\nFit g: {}".format(g_ref, g))
     plt.show()
 
-def data_fit():
-    low_limit, high_limit = 2, 13  # best limits according to prefits
-    lande = Lande(low_limit, high_limit, b=3.6e-3)
-    fit(lande)
-
-def toy_data_fit():
-    lande = Lande(2, 13, gen_data=True, b=5e-3)
-    fit(lande)
-
 
 if __name__ == '__main__':
-    toy_data_fit()
+    lande = Lande(2, 13, gen_data=True, b=5e-3)
+    fit(lande)
